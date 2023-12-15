f3dee91a41fecbfda56a8f90ac517284312e6569
mu7ammad <twassul.com@gmail.com>
2023-12-15 15:33:20 +0200

    first commit

--- src/app/icons/page.tsx
+++ src/app/icons/page.tsx
@@ -1,5 +1,21 @@
 import Alemet from '@/components/Alemet'
-import React from 'react'
+import { Metadata } from "next";
+
+
+
+
+
+const meta = {
+  title: 'Icons',
+  description: 'شملول هو موقع رائد يقدم مجموعة شاملة من الأدوات والموارد المبتكرة للمصممين والمبرمجين. يتيح الموقع للمستخدمين الوصول إلى مجموعة لونية واسعة تتيح لهم تحديد مظهر فريد لمشاريعهم باستخدام ألوان متنوعة وجذابة. بالإضافة إلى ذلك، يتميز الموقع بمكتبة ضخمة تحتوي على 1000 أيقونة إبداعية، مما يوفر للمستخدمين مصدرًا غنيًا لتحسين تجربة المستخدم في تصميماتهم',
+}
+
+
+export const metadata: Metadata = {
+  title: meta.title,
+  description: meta.description,
+}
+
 
 export default function Icons() {
   return (
