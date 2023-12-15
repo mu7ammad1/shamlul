1410f9b67ad8a00c8f3f570e535b1b4b9c93a35b
mu7ammad <twassul.com@gmail.com>
2023-12-15 15:00:12 +0200

    first commit

--- src/app/fonts/page.tsx
+++ src/app/fonts/page.tsx
@@ -6,16 +6,9 @@ import { Metadata } from "next";
 
 
 
-
-const meta = {
+export const metadata = {
   title: 'Icons',
   description: 'شملول هو موقع رائد يقدم مجموعة شاملة من الأدوات والموارد المبتكرة للمصممين والمبرمجين. يتيح الموقع للمستخدمين الوصول إلى مجموعة لونية واسعة تتيح لهم تحديد مظهر فريد لمشاريعهم باستخدام ألوان متنوعة وجذابة. بالإضافة إلى ذلك، يتميز الموقع بمكتبة ضخمة تحتوي على 1000 أيقونة إبداعية، مما يوفر للمستخدمين مصدرًا غنيًا لتحسين تجربة المستخدم في تصميماتهم',
-}
-
-
-export const metadata = {
-  title: meta.title,
-  description: meta.description,
 }
 
 export default function Fonts() {
@@ -35,7 +28,7 @@ export default function Fonts() {
     setVisibleCards((prevVisibleCards) => prevVisibleCards + 12);
   };
 
-  const Lang = [ 'Arabic', 'English']
+  const Lang = ['Arabic', 'English']
 
   const fruitsData = ([
     [Lang[1], 'Alyermook', 7, "cairo", "ffff"],
